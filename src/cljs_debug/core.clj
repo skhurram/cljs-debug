eb440a0482fbe13b4bee1452195bde20620fd856
David Nolen <david.nolen@gmail.com>
2011-12-08 02:02:13 -0500

    * src/cljs_debug/core.clj: we can talk to the debugging protocol

--- src/cljs_debug/core.clj
+++ src/cljs_debug/core.clj
@@ -1,9 +1,42 @@
 (ns cljs-debug.core
-  (:use [clojure.data.json :only [read-json]])
-  (import [java.net URL]
+  (:use [clojure.data.json :only [json-str read-json]])
+  (import [java.net URL URI]
+          [org.jboss.netty.handler.codec.http.websocket
+           WebSocketFrame
+           DefaultWebSocketFrame]
           [se.cgbystrom.netty.http.websocket
            WebSocketCallback
            WebSocketClient
            WebSocketClientFactory]))
 
-(read-json (slurp (URL. "http://localhost:9222/json")))
+(deftype WSCallback []
+  WebSocketCallback
+  (^void onConnect [this ^WebSocketClient c]
+    (println "connected!"))
+  (^void onDisconnect [this ^WebSocketClient c]
+    (println "disconnected!"))
+  (^void onMessage [this ^WebSocketClient c ^WebSocketFrame f]
+    (println "message" (.getTextData f)))
+  (^void onError [this ^Throwable t]
+    (.printStackStrace t)))
+
+(defn ^DefaultWebSocketFrame frame [s]
+  (DefaultWebSocketFrame. s))
+
+(defn ^WebSocketClient make-client []
+  (let [f (WebSocketClientFactory.)
+        cb (WSCallback.)]
+    (.newClient f (URI. "ws://localhost:9222/devtools/page/1") cb)))
+
+(comment
+  (def rpc {"id" 0
+            "method" "Runtime.evaluate"
+            "params" {"expression" "4+5"
+                      "returnByValue" true}})
+
+  (def c (make-client))
+  (.connect c)
+  (.send c (frame (json-str rpc)))
+  (.disconnect c)
+  (read-json (slurp (URL. "http://localhost:9222/json")))
+  )
