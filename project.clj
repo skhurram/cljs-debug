4ebb5fccac55ac68092206a0ef9468bd4a15410b
David Nolen <david.nolen@gmail.com>
2011-12-08 01:43:51 -0500

    * project.clj: sort out deps

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject cljs-debug "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.jboss.netty/netty "3.2.7.Final"]
+                 [com.cgbystrom/netty-tools "1.2.7"]
                  [org.clojure/data.json "0.1.1"]])
--- src/cljs_debug/core.clj
+++ src/cljs_debug/core.clj
@@ -1,5 +1,9 @@
 (ns cljs-debug.core
   (:use [clojure.data.json :only [read-json]])
-  (import [java.net URL]))
+  (import [java.net URL]
+          [se.cgbystrom.netty.http.websocket
+           WebSocketCallback
+           WebSocketClient
+           WebSocketClientFactory]))
 
 (read-json (slurp (URL. "http://localhost:9222/json")))
